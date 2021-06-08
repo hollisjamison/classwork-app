fb0d9ef50b5529b9ac2a30db8f0de6eaafb52b92
hollisjamison <hollisjamison@gmail.com>
2021-06-08 05:42:46 +0000

    Working create react app and express app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const buildPath = path.join(__dirname, 'client', 'build')
+
+const apiRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(buildPath));
+
+app.use('/api', apiRouter);
+
+app.use('/', (req, res) => {
+ res.sendFile(sendPath)
+});
+
+module.exports = app;
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="react-app">
+      React app working!
+    </div>
+  );
+}
+
+export default App;
