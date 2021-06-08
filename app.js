e7111f526fb39bb515ed6cf8f9e8d1b901ae0938
hollisjamison <hollisjamison@gmail.com>
2021-06-08 05:53:49 +0000

    Removes www folder and simplifies port

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const port = process.env.PORT || '3001'
 
-const buildPath = path.join(__dirname, 'client', 'build')
+const buildPath = path.join(__dirname, "client", "build");
 
-const apiRouter = require('./routes/api');
+const apiRouter = require("./routes/api");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(buildPath));
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.use('/', (req, res) => {
- res.sendFile(sendPath)
+app.use("/", (req, res) => {
+  res.sendFile(sendPath);
 });
+
+app.listen(port, () => {
+    console.log(`Backend is listening @ http://localhost:${port}`)
+})
 
 module.exports = app;
