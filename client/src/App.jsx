9c0d94086d390182bcf1383f7136ebe229605e72
hollisjamison <hollisjamison@gmail.com>
2021-06-08 17:31:33 +0000

    First steps of github authentication on react side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import Login from './components/Login'
+
 const App = () => {
   return (
     <div className="react-app">
-      React app working!
+      <Login />
     </div>
   );
 }
